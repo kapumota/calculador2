f8aaf9d8d19aa32fef0a6268d538eb865bb367d3
kapumota <claraa@uni.edu.pe>
2022-12-28 12:54:13 -0500

    Correccion 1

--- src/main/java/com/kapumota/calculador/CalculadorApplication.java
+++ src/main/java/com/kapumota/calculador/CalculadorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadorApplication {
-    private static final String constant = "constant";
+    //private static final String constant = "constant";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadorApplication.class, args);
